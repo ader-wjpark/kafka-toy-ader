58c53d6339157e6712ffe4ae146f3a22e21b7a9b
ader-wjpark <n210207@N210207ui-MacBookPro.local>
2022-11-08 16:48:21 +0900

    init: 프로젝트 최초 세팅

---
+++ src/main/java/com/example/kafkatoyader/KafkaToyAderApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkatoyader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaToyAderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToyAderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkatoyader/KafkaToyAderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatoyader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaToyAderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
